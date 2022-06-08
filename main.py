6e8043f80800f6b2cea02e64118eb99d7462dc42
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-06-08 22:48:20 +0700

    the twoSum function is done

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                result = [i, j]
+                return result
 
+def check_int_number():
+    """
+    Проверяем является ли символ целым числом.
+    :return:
+    """
+    pass
+
+def get_numbers():
+    """
+    Из строки получаем числа
+    -10^9 <= number <= 10^9
+    -10^9 <= target <= 10^9
+    :return: List[int]
+    """
+    pass
 
 
 if __name__ == '__main__':
-    pass
+
+    print('Enter numbers, segregate with , ')
+    numbers = input()
+    print('Enter the target number')
+    target = int(input())
