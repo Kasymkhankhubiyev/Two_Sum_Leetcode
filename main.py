58f48ce8fe449976903b358805af693581d44ac3
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-06-08 22:56:10 +0700

    commit changes

--- main.py
+++ main.py
@@ -17,19 +17,42 @@ def check_int_number():
     """
     pass
 
-def get_numbers():
+def get_numbers(string):
     """
     Из строки получаем числа
     -10^9 <= number <= 10^9
     -10^9 <= target <= 10^9
     :return: List[int]
     """
-    pass
+    numbers = string.split(', ')
+    for num in numbers:
+        print(num)
+    return  numbers
+
+def check_exit(string):
+    """
+    Функция выхода из программы
+    :param string:
+    :return:
+    """
+    if string == 'Q' or string == 'q':
+        return False
+    else:
+        return True
 
 
 if __name__ == '__main__':
+    run = True
 
-    print('Enter numbers, segregate with , ')
-    numbers = input()
-    print('Enter the target number')
-    target = int(input())
+    while run:
+        print('Enter numbers, segregate with , ')
+        numbers = input()
+        print('Enter the target number')
+        target = int(input())
+
+        nums = get_numbers(numbers)
+
+        print('Press Q to exit, and any to continue')
+        state = input()
+        run = check_exit(state)
+
